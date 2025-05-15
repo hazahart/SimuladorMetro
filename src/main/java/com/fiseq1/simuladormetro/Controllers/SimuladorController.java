f1e92578a379b340762835785428d66a09234a22
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2025-05-14 18:04:18 -0600

    Adicion de la clase SimuladorController

---
+++ src/main/java/com/fiseq1/simuladormetro/Controllers/SimuladorController.java
@@ -0,0 +1,10 @@
+package com.fiseq1.simuladormetro.Controllers;
+
+import com.fiseq1.simuladormetro.Views.SimuladorView;
+
+public class SimuladorController {
+    private SimuladorView simulador;
+    public SimuladorController(SimuladorView vista) {
+        this.simulador = vista;
+    }
+}
